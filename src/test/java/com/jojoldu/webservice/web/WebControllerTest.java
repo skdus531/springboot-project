d2a2ae260fb59e8f4ba479cb73a8cb296a433b03
nayeon <skdus531@daum.net>
2021-01-19 15:25:58 +0900

    change test

--- src/test/java/com/jojoldu/webservice/web/WebControllerTest.java
+++ src/test/java/com/jojoldu/webservice/web/WebControllerTest.java
@@ -29,6 +29,6 @@ public class WebControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+        assertThat(body).contains("주가 예측 웹 서비스");
     }
 }
