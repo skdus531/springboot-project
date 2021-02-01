29bd8d76e75c0d6d33d66e419cce6e77712598fb
nayeon <skdus531@daum.net>
2021-02-01 12:37:09 +0900

    rds2

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,3 +1,3 @@
-insert into posts (title, author, content, created_date, modified_date) values ('삼성전자', 87000, '테스트1의 본문', now(), now());
-insert into posts (title, author, content, created_date, modified_date) values ('네이버', 347500, '테스트2의 본문', now(), now());
-insert into posts (title, author, content, created_date, modified_date) values ('LG', 347500, '테스트3의 본문', now(), now());
+insert into posts (title, author, content, created_date, modified_date) values ('삼성전자', 87000, '005930', now(), now());
+insert into posts (title, author, content, created_date, modified_date) values ('NAVER', 347500, '035420', now(), now());
+insert into posts (title, author, content, created_date, modified_date) values ('LG', 347500, '003550', now(), now());
