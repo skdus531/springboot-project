9365b294d2a638387c034b57cc7c7a5516291a5b
nayeon <skdus531@daum.net>
2021-02-01 14:42:02 +0900

    predict1

--- src/main/resources/static/js/app/main.js
+++ src/main/resources/static/js/app/main.js
@@ -26,7 +26,7 @@ var main = {
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
-            alert('글이 등록되었습니다.');
+            alert('회사가 정상적으로 등록되었습니다.');
             location.reload();
         }).fail(function (error) {
             alert(error);
